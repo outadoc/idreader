c37fc8be4f0a7b5bec29350b8f1ac390832aab22
Baptiste Candellier <baptiste@candellier.me>
2026-07-08 22:36:23 +0200

    feat: add another example

--- shared/src/androidHostTest/kotlin/fr/outadoc/eidas/lds/ParseMrzUseCaseTest.kt
+++ shared/src/androidHostTest/kotlin/fr/outadoc/eidas/lds/ParseMrzUseCaseTest.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 
 class ParseMrzUseCaseTest {
     @Test
-    fun `Parse specimen MRZ`() {
+    fun `Parse TD1 MRZ from FR specimen`() {
         val mrz =
             "IDFRAX4RTBPFW46<<<<<<<<<<<<<<<9007138F3002119FRA<<<<<<<<<<<6MARTIN<<MAELYS<GAELLE<MARIE<<<"
 
@@ -35,6 +35,40 @@ class ParseMrzUseCaseTest {
                     sex = "F",
                     expiryDate = "300211",
                     optionalData1 = null,
+                    optionalData2 = null,
+                ),
+            actual = parsed,
+        )
+    }
+
+    @Test
+    fun `Parse TD1 MRZ from ISAO example`() {
+        val mrz = "I<NLDXI85935F86999999990<<<<<<7208148F1108268NLD<<<<<<<<<<<4VAN<DER<STEEN<<MARIANNE<LOUISE"
+
+        val parseMrz =
+            ParseMrzUseCase(
+                parseMrzName = ParseMrzNameUseCase(),
+            )
+
+        val parsed = parseMrz(mrz).getOrThrow()
+
+        assertEquals(
+            expected =
+                MrzInfo(
+                    format = "TD1",
+                    documentCode = "I<",
+                    documentNumber = "XI85935F8",
+                    issuingState = "NLD",
+                    nationality = "NLD",
+                    cardHolderName =
+                        CardHolderName(
+                            surname = "VAN DER STEEN",
+                            givenNames = listOf("MARIANNE", "LOUISE"),
+                        ),
+                    birthDate = "720814",
+                    sex = "F",
+                    expiryDate = "110826",
+                    optionalData1 = "999999990",
                     optionalData2 = null,
                 ),
             actual = parsed,
