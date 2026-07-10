bca0ea29a4f0dbdfad1684a0b1dc634e389adac0
Baptiste Candellier <baptiste@candellier.me>
2026-07-10 21:56:40 +0200

    feat: flatten result calls in ParseDG1UseCase

--- shared/src/commonMain/kotlin/fr/outadoc/eidas/lds/ParseDG1UseCase.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/eidas/lds/ParseDG1UseCase.kt
@@ -2,7 +2,6 @@ package fr.outadoc.eidas.lds
 
 import fr.outadoc.eidas.lds.model.MrzInfo
 import fr.outadoc.eidas.nfc.Iso7816
-import fr.outadoc.eidas.tlv.TlvNode
 import fr.outadoc.eidas.tlv.firstWithTag
 import fr.outadoc.eidas.tlv.parseTlv
 import fr.outadoc.eidas.utils.flatMap
@@ -11,28 +10,18 @@ import fr.outadoc.eidas.utils.flatMap
 class ParseDG1UseCase(
     private val parseMrzUseCase: ParseMrzUseCase,
 ) {
-    operator fun invoke(rawData: UByteArray): Result<MrzInfo> {
-        val tagList: List<TlvNode> =
-            rawData
-                .parseTlv()
-                .getOrElse {
-                    return Result.failure(it)
-                }
-
-        val rootNode: TlvNode =
-            tagList
-                .firstWithTag(Iso7816.Tags.DG1)
-                .getOrElse { return Result.failure(it) }
-
-        val mrzBytes: UByteArray =
-            rootNode.value
-                .parseTlv()
-                .flatMap { nodes -> nodes.firstWithTag(Iso7816.Tags.MRZ) }
-                .getOrElse { return Result.failure(it) }
-                .value
-
-        val mrz: String = mrzBytes.toByteArray().decodeToString()
-
-        return parseMrzUseCase(mrz)
-    }
+    operator fun invoke(rawData: UByteArray): Result<MrzInfo> =
+        rawData
+            .parseTlv()
+            .flatMap { tagList -> tagList.firstWithTag(Iso7816.Tags.DG1) }
+            .flatMap { rootNode -> rootNode.value.parseTlv() }
+            .flatMap { nodes -> nodes.firstWithTag(Iso7816.Tags.MRZ) }
+            .flatMap { mrzNode ->
+                parseMrzUseCase(
+                    mrz =
+                        mrzNode.value
+                            .toByteArray()
+                            .decodeToString(),
+                )
+            }
 }
