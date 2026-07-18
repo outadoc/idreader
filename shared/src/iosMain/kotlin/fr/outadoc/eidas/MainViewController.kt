06901e4e93b1ecc24be97f78da8de0760333dec1
Baptiste Candellier <baptiste.candellier@dejamobile.com>
2026-07-18 22:09:53 +0200

    feat: init keyer and fetcher

--- shared/src/iosMain/kotlin/fr/outadoc/eidas/MainViewController.kt
+++ shared/src/iosMain/kotlin/fr/outadoc/eidas/MainViewController.kt
@@ -11,6 +11,8 @@ import fr.outadoc.eidas.di.iosModule
 import fr.outadoc.eidas.di.sharedModule
 import fr.outadoc.eidas.logging.Logger
 import fr.outadoc.eidas.media.CoilLogger
+import fr.outadoc.eidas.media.DocumentPictureFetcher
+import fr.outadoc.eidas.media.DocumentPictureKeyer
 import fr.outadoc.eidas.media.Jpeg2000Decoder
 import fr.outadoc.eidas.settings.SettingsEncryptor
 import org.koin.compose.KoinApplication
@@ -44,6 +46,8 @@ fun MainViewController(
                     .crossfade(true)
                     .logger(CoilLogger(logger))
                     .components {
+                        add(DocumentPictureKeyer())
+                        add(DocumentPictureFetcher.Factory())
                         add(Jpeg2000Decoder.Factory())
                     }.build()
             }
