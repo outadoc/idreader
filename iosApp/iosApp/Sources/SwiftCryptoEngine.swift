e0c7e4c01b09fe82acec7f016525f7486be8b177
Baptiste Candellier <baptiste.candellier@dejamobile.com>
2026-07-18 20:00:14 +0200

    chore: update swift impl

--- iosApp/iosApp/Sources/SwiftCryptoEngine.swift
+++ iosApp/iosApp/Sources/SwiftCryptoEngine.swift
@@ -8,35 +8,35 @@
 import Shared
 
 class SwiftCryptoEngine: CryptoEngine {
-    func computeCmac(algorithm: Algorithm, key: Any, data: Any) -> Any {
+    func computeCmac(algorithm: Algorithm, key: KmpBytes, data: KmpBytes) -> KmpBytes {
         fatalError()
     }
     
-    func computeMappedGenerator(algorithm: Algorithm, mappingPrivateKey: any PrivateKey, chipMappingPublicPoint: EcPoint, decryptedNonce: Any) -> EcPoint {
+    func computeMappedGenerator(algorithm: Algorithm, mappingPrivateKey: any PrivateKey, chipMappingPublicPoint: EcPoint, decryptedNonce: KmpBytes) -> EcPoint {
         fatalError()
     }
     
-    func computeSha1(message: Any) -> Any {
+    func computeSha1(message: KmpBytes) -> KmpBytes {
         fatalError()
     }
     
-    func computeSharedSecret(algorithm: Algorithm, privateKey: any PrivateKey, chipPublicPoint: EcPoint) -> Any {
+    func computeSharedSecret(algorithm: Algorithm, privateKey: any PrivateKey, chipPublicPoint: EcPoint) -> KmpBytes {
         fatalError()
     }
     
-    func decryptSymmetric(algorithm: Algorithm, key: Any, data: Any) -> Any {
+    func decryptSymmetric(algorithm: Algorithm, key: KmpBytes, data: KmpBytes) -> KmpBytes {
         fatalError()
     }
     
-    func decryptSymmetricWithIv(algorithm: Algorithm, key: Any, iv: Any, data: Any) -> Any {
+    func decryptSymmetricWithIv(algorithm: Algorithm, key: KmpBytes, iv: KmpBytes, data: KmpBytes) -> KmpBytes {
         fatalError()
     }
     
-    func deriveKeyFromSecret(algorithm: Algorithm, secret: Any, nonce: Any, counter: Int32) -> Any {
+    func deriveKeyFromSecret(algorithm: Algorithm, secret: KmpBytes, nonce: KmpBytes, counter: Int32) -> KmpBytes {
         fatalError()
     }
     
-    func encryptSymmetric(algorithm: Algorithm, key: Any, iv: Any, data: Any) -> Any {
+    func encryptSymmetric(algorithm: Algorithm, key: KmpBytes, iv: KmpBytes, data: KmpBytes) -> KmpBytes {
         fatalError()
     }
 }
